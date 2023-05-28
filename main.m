c982f07a30050a4081b25ef29daab8991442eaa2
Habiba <133682363+HabibaAdelAly@users.noreply.github.com>
2023-05-28 18:44:39 +0300

    Completed

--- main.m
+++ main.m
@@ -3,137 +3,149 @@ clear;
 close all;
 
 %Getting the video
-vid=VideoReader('Stephan.avi');
+vid=VideoReader('Stephan.avi.mp4');
 
 %Getting the number of frames in the vedio
 Frames_num = vid.NumFrames;
 frame_rate = vid.FrameRate;
 
+% Create a VideoWriter object with a specified filename and codec
+outputVideo = VideoWriter('output.avi', 'Uncompressed AVI');
+
+%create error Ranges and probabilities array
+errorsRange= 0.0001:0.001:0.2;
+errorProbability=[];
+
 %Creating decoded frames
 decodedFrames={};
 
 %Creating the trellis (change the numbers however u like :))
-K=7;
-trellis=poly2trellis(K, [133 177]);
-
-%defining the probability of error (also change it however u like :))
-pe=0.1;
+trellis = poly2trellis(7,[135 135 147 163]);
 
 %deining if the channel uses channel coding or not
-isChannelCoding=true;
+isChannelCoding=1;
+
+for j=1:1:size(errorsRange,2)
+counter=0;
+
+%defining the probability of error (also change it however u like :))
+pe=errorsRange(j);
 
 %getting components for each color as binary
 for i=1:Frames_num
     frame = read(vid, i);
-     %getting  red components 
-    redComponent = frame(:,:,1);
-     %getting  green components
-    greenComponent = frame(:,:,2);
-     %getting  blue components
-    blueComponent = frame(:,:,3);
+   %getting  red components 
+     redComponentframe = frame(:,:,1);
+   %getting  green components
+     greenComponentframe = frame(:,:,2);
+   %getting  blue components
+     blueComponentframe = frame(:,:,3);
 
-redComponent=reshape(redComponent,1,[]); %we reshape to put the red as a stream of 1 row 
-greenComponent=reshape(greenComponent,1,[]); %we reshape to put the green as a stream of 1 row 
-blueComponent=reshape(blueComponent,1,[]); %we reshape to put the blue as a stream of 1 row 
+redComponent=reshape(redComponentframe,1,[]); %we reshape to put the red as a stream of 1 row 
+greenComponent=reshape(greenComponentframe,1,[]); %we reshape to put the green as a stream of 1 row 
+blueComponent=reshape(blueComponentframe,1,[]); %we reshape to put the blue as a stream of 1 row 
 
 redComponentdec=double(redComponent);%convert red map-8 to decimal
 greenComponentdec=double(greenComponent);%convert green map-8 to decimal
 blueComponentdec=double(blueComponent);%convert blue map-8 to decimal
 
-Rbin=dec2bin(redComponentdec,8); %convert red decimal to binary in 8 bits
-Gbin=dec2bin(greenComponentdec,8); %convert green decimal to binary in 8 bits
-Bbin=dec2bin(blueComponentdec,8); %convert blue decimal to binary in 8 bits
+Rbin=de2bi(redComponentdec,8);%convert red decimal to binary in 8 bits
+Gbin=de2bi(greenComponentdec,8);%convert green decimal to binary in 8 bits
+Bbin=de2bi(blueComponentdec,8);%convert blue decimal to binary in 8 bits
 
-
-%double converts the input string to a numeric array of ASCII codes. The
-%ASCII of zero is 48 so we subtract by 48
-Rbin_stream=double(reshape(Rbin,[1,101376*8]))-48;%convert the char into an array Red component
-Gbin_stream=double(reshape(Rbin,[1,101376*8]))-48;%convert the char into an array green component
-Bbin_stream=double(reshape(Rbin,[1,101376*8]))-48;%convert the char into an array blue component
-
-if isChannelCoding==true
-%Red 
-decodedRed = transmit_channel_coding(Rbin_stream,trellis,pe,K);
-
-%Green
-decodedGreen = transmit_channel_coding(Gbin_stream,trellis,pe,K);
-
-%Blue
-decodedBlue = transmit_channel_coding(Bbin_stream,trellis,pe,K);
-
-else
-
-%Red 
-decodedRed = transmit(Rbin_stream,pe);
-
-%Green
-decodedGreen = transmit(Gbin_stream,pe);
-
-%Blue
-decodedBlue = transmit(Bbin_stream,pe);
+%with channel coding
+if (isChannelCoding==1)
+decodedRed  =transmit_channel_coding(Rbin,trellis,pe); %red
+decodedGreen=transmit_channel_coding(Gbin,trellis,pe); %green
+decodedBlue =transmit_channel_coding(Bbin,trellis,pe); %blue
 end
+
+%without channel coding
+if (isChannelCoding==0)
+decodedRed=introduceBitErrors(pe,Rbin); %red
+decodedGreen=introduceBitErrors(pe,Gbin); %green
+decodedBlue=introduceBitErrors(pe,Bbin); %blue
+end
+
+%getting the number of bits in error after decoding
+counter=counter+sum(sum(xor(decodedRed,Rbin)))+sum(sum(xor(decodedGreen,Gbin)))+sum(sum(xor(decodedBlue,Bbin)));
+
+decodedRedDec=bi2de(decodedRed);%convert decoded red binary to decimal
+decodedGreenDec=bi2de(decodedGreen);%convert decoded green binary to decimal
+decodedBlueDec=bi2de(decodedBlue);%convert decoded blue binary to decimal
+
+decodedRedUint8=uint8(decodedRedDec);%convert decoded Red decimal into uint8 format
+decodedGreenUint8=uint8(decodedGreenDec);%convert decoded green decimal into uint8 format
+decodedBlueUint8=uint8(decodedBlueDec);%convert decoded blue decimal into uint8 format
+
+Rframe=reshape(decodedRedUint8,size(frame,1),size(frame,2),[]); %we reshape to put the red in the original size
+Gframe=reshape(decodedGreenUint8,size(frame,1),size(frame,2),[]);%we reshape to put the green in the original size
+Bframe=reshape(decodedBlueUint8,size(frame,1),size(frame,2),[]);%we reshape to put the blue in the original size
 
 %combine color components into a single frame
-decodedFrame = cat(3, decodedRed, decodedGreen, decodedBlue);
+decodedFrame = cat(3, Rframe,Gframe, Bframe);
 
 %store decoded frame in cell array
-decodedFrames=cat(2,decodedFrames,decodedFrame) ;
-
+decodedFrames = cat(2,decodedFrames,decodedFrame);
+ 
 end
 
-% create a VideoWriter object with a specified filename and frame rate
-outputVideo = VideoWriter('output.avi','Motion JPEG 2000');
+%getting the probability of error
+probability=counter/(Frames_num*(numel(Rbin)*3));
+errorProbability=[errorProbability,probability];
+
+%getting the rates of the codes (trellis number could be changed)
+rate=numel(Rbin)/numel(toEncode(Rbin,trellis));
+
+end
 
 % open the video for writing
 open(outputVideo);
 
-% write each frame to the video
-for i = 1:Frames_num
-   writeVideo(outputVideo, decodedFrames{i});
+%writing the frames in the outputVideo
+for i=1:Frames_num
+frame=decodedFrames{i};
+writeVideo(outputVideo, frame);
 end
 
+%close the video
 close(outputVideo);
 
-%Here starts the functions that encodes, decodes and puts the error
+% plot error probability vs. error range
+figure;
+plot(errorsRange, errorProbability);
+xlabel('Error Range');
+ylabel('Error Probability');
+title('Error Probability vs. Error Range');
 
+
+%Here starts the functions that encodes, decodes and puts the error
 %1)Getting the Trellis with different rates and then encoding
-function [encodedData] = toEncode(data,trellis)
-encodedData=convenc(data(1,:),trellis);
-encodedData=reshape(encodedData,[(length(encodedData)/8),8]);
+function [encodedData] = toEncode(data2,trellis)
+data= reshape(data2, [], 1);
+encodedData=convenc(data,trellis);
+encodedData=reshape(encodedData,size(data2,1),[]);
 end
 
 %2)Sending the bits in error/error_free
-function [errorStream]= getErrorStream(probabilityOfError,correctStream)
-errorStream=correctStream;
-for i=1:length(correctStream)
-    random=rand;
-    if(random<=probabilityOfError)
-        if(correctStream(i)==0)
-            errorStream(i)=1;
-        else
-            errorStream(i)=0;
-        end
-    end
-end
+function [errorStream] = introduceBitErrors(probabilityOfError, correctStream)
+    random = rand(size(correctStream)); % generate random matrix
+    errorStream = correctStream; % initialize output matrix
+    % compare random matrix with error probability matrix and flip bits
+    errorStream(random < probabilityOfError) = 1 - errorStream(random < probabilityOfError);
 end
 
 %3)Decoding using a specific trellis
-function [decodedData] = toDecode(data,K,encodedData,trellis)
-number_of_bits=numel(encodedData);
-encodedData=reshape(encodedData,[1,number_of_bits]);
-tbDepth=round((K-1)/(1-(length(data)/length(encodedData))));
-decodedData =vitdec(encodedData(1,:),trellis,tbDepth,'trunc','hard');
-decodedData=reshape(decodedData,[(length(decodedData)/8),8]);
+function [decodedData] = toDecode(data2,trellis)
+%number_of_bits=numel(encodedData);
+data= reshape(data2, [], 1);
+decodedData =vitdec(data,trellis,50,'trunc','hard');
+decodedData=reshape(decodedData,size(data2,1),[]);
 end
 
 %4)Function that encodes, decodes and puts error
-function[decodedData]=transmit_channel_coding(data,trellis,error,K)
+function[decodedData]=transmit_channel_coding(data,trellis,error)
 encodedData=toEncode(data,trellis);
-errorData=getErrorStream(error,encodedData);
-decodedData= toDecode(data,K,errorData,trellis);
-end
-
-%5)Function that transmits without channel coding (only error)
-function[sentData]=transmit(data,error)
-sentData=getErrorStream(error,data);
+errorData=introduceBitErrors(error,encodedData);
+decodedData= toDecode(errorData,trellis);
 end
